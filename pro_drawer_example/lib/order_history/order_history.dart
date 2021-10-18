01f51e61496525ddfd01c610c054c91d663e992d
Ashiq <ashiqul.islam@sslwireless.com>
2021-10-18 09:35:21 +0600

    Second page added for example

--- pro_drawer_example/lib/order_history/order_history.dart
+++ pro_drawer_example/lib/order_history/order_history.dart
@@ -1,11 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:pro_drawer/pro_drawer.dart';
 
 class OrderHistory extends StatelessWidget {
   const OrderHistory({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {
+            ProDrawer.of(context).toggle();
+          },
+          icon: const Icon(
+            Icons.menu,
+          ),
+        ),
+      ),
       backgroundColor: Colors.amber,
     );
   }
